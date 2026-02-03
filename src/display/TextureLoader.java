bb68d679c278b795fc42ef1d8a234a57c2619b23
liamreal <liam.kerrin@ucdconnect.ie>
2026-02-03 12:04:11 +0000

    added assets path to texture loader class

--- src/display/TextureLoader.java
+++ src/display/TextureLoader.java
@@ -3,7 +3,11 @@ package display;
 // will be responsible for obtaining the directory whose textures are used
 public class TextureLoader {
     private static String gameType = "default";
+    private static String assetsPath = String.format("assets/%s", gameType);
 
     public static String getGameType() {return gameType;}
+    public static String getAssetsPath() {return assetsPath;}
+
     public static void setGameType(String newGameType) {gameType = newGameType;}
+
 }
