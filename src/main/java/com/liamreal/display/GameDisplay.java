4d37ef9d59cabd09f33662bbf5265cda9f6420eb
liamreal <liam.kerrin@ucdconnect.ie>
2026-03-03 11:49:38 +0000

    made GameDisplay fields final since these are game display values for rendering and NOT resolution values from config

--- src/main/java/com/liamreal/display/GameDisplay.java
+++ src/main/java/com/liamreal/display/GameDisplay.java
@@ -4,13 +4,13 @@ import com.liamreal.util.Point3f;
 
 public class GameDisplay {
 
-	private static int DISPLAY_X = 854;
-	private static int DISPLAY_Y = 480;
+	private static final int DISPLAY_X = 854;
+	private static final int DISPLAY_Y = 480;
 	
-	public static int getDisplayX() {return DISPLAY_X;}
-	public static int getDisplayY() {return DISPLAY_Y;};
-	// get centre of screen (useful if placing a new item)
-	public static Point3f getDisplayCentre() {return new Point3f(getDisplayX()/2,getDisplayY()/2,0);}
+	public static int getDisplayX() { return DISPLAY_X; }
+	public static int getDisplayY() { return DISPLAY_Y; }
+	// get centre of screen (useful for placing player in centre of game)
+	public static Point3f getDisplayCentre() { return new Point3f(getDisplayX()/2,getDisplayY()/2,0); }
 
 
 	
