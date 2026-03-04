2bdbcba4e6e03b1ae24b99d316a4d387ad092ee3
liamreal <liam.kerrin@ucdconnect.ie>
2026-03-04 15:00:36 +0000

    reworked sound code to have less repetitive code

--- src/main/java/com/liamreal/assets/SoundPlayer.java
+++ src/main/java/com/liamreal/assets/SoundPlayer.java
@@ -4,45 +4,44 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
-// method idea obtained from ChatGPT, instead of texture, sounds will be more dynamic and can be inserted at any point, 
+// similar to texture, but sounds will be more dynamic and can be inserted at any point, 
 // HOWEVER they will need to be passed in the sound file as a string, of which path can be obtained from AssetLoader class
 public class SoundPlayer {
     // static method that any class can use to play sound
-    public static void playSound(String path) {
-        try {
-            File file = new File(path);
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-
-            Clip clip = AudioSystem.getClip();
-            clip.open(audioStream);
-            clip.start();
-            audioStream.close();
-
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
+    public static Clip playSound(String path) {
+        return playSound(path, false);
     }
-    // different method to loop sound
-    public static Clip loopSound(String path) {
+    public static Clip playSound(String path, boolean loopSound) {
         try {
             File file = new File(path);
+            // get audio stream for audio from input file
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-
+            // save clip from stream
             Clip clip = AudioSystem.getClip();
+            // open audio stream clip, start playing clip and close audio stream
             clip.open(audioStream);
-            
-            // reduced volume for looping clips
-            FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-            volume.setValue(-10.0f);
-
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            // if want to loop sound loop but also reduce volume or music will get annoying
+            if (loopSound) {
+                // reduced volume for looping clips
+                FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                volume.setValue(-10.0f);
+                // loop clip conitnuously
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            } else {
+                // otherwise just start sound
+                clip.start();
+            }
+            audioStream.close();
             return clip;
 
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
-        // if failed to play sound return null
         return null;
+    }
+    // different method to loop sound
+    public static Clip loopSound(String path) {
+        return playSound(path, true);
     }
     // stop sound
     public static void stopSound(Clip clip) {
