5146b13e507e759c1134670b4e53a5ab2cc8c289
liamreal <liam.kerrin@ucdconnect.ie>
2026-06-10 12:12:20 +0100

    fixed a bug with final reference for mutable object in GameDisplay, reverted change to calculating display centre instead of storing the calculated value because was messing with player controls

--- src/main/java/com/liamreal/display/GameDisplay.java
+++ src/main/java/com/liamreal/display/GameDisplay.java
@@ -6,12 +6,11 @@ public class GameDisplay {
 
 	private static final int DISPLAY_X = 854;
 	private static final int DISPLAY_Y = 480;
-	private static final Point3f DISPLAY_CENTRE = new Point3f(getDisplayX()/2,getDisplayY()/2,0);
 	
 	public static int getDisplayX() { return DISPLAY_X; }
 	public static int getDisplayY() { return DISPLAY_Y; }
 	// get centre of screen (useful for placing player in centre of game)
-	public static Point3f getDisplayCentre() { return DISPLAY_CENTRE; }
+	public static Point3f getDisplayCentre() { return new Point3f(getDisplayX()/2,getDisplayY()/2,0); }
 
 
 	
