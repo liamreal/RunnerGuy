51682afb0fc462fc57550d49e36745749a988645
liamreal <liam.kerrin@ucdconnect.ie>
2026-07-20 16:22:56 +0200

    renamed AssetLoader to AssetSelector to better represent its function of selecting directory

--- src/main/java/com/liamreal/MainWindow.java
+++ src/main/java/com/liamreal/MainWindow.java
@@ -15,7 +15,7 @@ import javax.sound.sampled.*;
 
 import com.liamreal.display.GameDisplay;
 import com.liamreal.user.Config;
-import com.liamreal.assets.AssetLoader;
+import com.liamreal.assets.AssetSelector;
 import com.liamreal.assets.SoundPlayer;
 import com.liamreal.controllers.Controller;
 
@@ -87,7 +87,7 @@ public class MainWindow {
 	        //loading background image 
 	        File BackgroundToLoad = new File(String.format(
 				"assets/start_menu.png", 
-				AssetLoader.getAssetsPath()
+				AssetSelector.getAssetsPath()
 			));  //should work okay on OSX and Linux but check if you have issues depending your eclipse install or if your running this without an IDE 
 			try {
 				 
@@ -109,12 +109,12 @@ public class MainWindow {
 		MainWindow hello = new MainWindow();  //sets up environment 
 
 		for (String level: Config.getInstance().getLevels()) {
-			AssetLoader.setGameType(level);
+			AssetSelector.setGameType(level);
 			boolean levelComplete = false;
 			// loop background music clip
 			Clip musicClip = SoundPlayer.loopSound(String.format(
 				"%s/sounds/background/background.wav",
-				AssetLoader.getAssetsPath()
+				AssetSelector.getAssetsPath()
 			));
 			while(!levelComplete)   //not nice but remember we do just want to keep looping till the end.  // this could be replaced by a thread but again we want to keep things simple 
 			{ 
--- src/main/java/com/liamreal/assets/AssetSelector.java
+++ src/main/java/com/liamreal/assets/AssetSelector.java
@@ -2,11 +2,11 @@
 package com.liamreal.assets;
 
 // will be responsible for obtaining the directory whose textures are used
-public class AssetLoader {
+public class AssetSelector {
     private static String gameType = "default";
 
     public static String getGameType() { return gameType; }
-    public static String getAssetsPath() { return String.format("assets/%s", AssetLoader.getGameType()); }
+    public static String getAssetsPath() { return String.format("assets/%s", AssetSelector.getGameType()); }
 
     // to switch between different game types (the idea is to later allow easy switching of assets for recyclable gameplay)
     public static void setGameType(String newGameType) { gameType = newGameType; }
