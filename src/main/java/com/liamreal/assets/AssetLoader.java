6fee7d69e7e5eb403e7bc31177de5ae4dfb25ea4
liamreal <liam.kerrin@ucdconnect.ie>
2026-07-20 20:32:11 +0200

    changed design of AssetLoader.buildTextures to just build texture map instead of creating the field then returning, updates field in class instead

--- src/main/java/com/liamreal/assets/AssetLoader.java
+++ src/main/java/com/liamreal/assets/AssetLoader.java
@@ -7,21 +7,20 @@ import java.util.Map;
 
 import com.liamreal.components.graphics.ImageLoader;
 
+// make this a singleton
 // will be responsible for obtaining the directory whose textures are used
 public class AssetLoader {
+    // will hold our actual images associated with keys
+    private Map<String, BufferedImage> textureMap = new HashMap<>();
 
-    // make this a singleton
-
-    public Map<String, BufferedImage> buildTextureMap() {
-        // will hold our actual images associated with keys
-        Map<String, BufferedImage> textureMap = new HashMap<>();
+    // in constructor, will build texture map and then once game is changed will update it via string formatting
+    public void buildTextureMap() {
         // build path map of textures
         Map<String, Path> pathMap = PathBuilder.buildPathMap(AssetSelector.getTexturesPath());
         // for each texture found in path, load it and add that loaded image as value to map with same keys
         for (Map.Entry<String, Path> entry : pathMap.entrySet()) {
-            textureMap.put(entry.getKey(), ImageLoader.load(entry.getValue()));
+            this.textureMap.put(entry.getKey(), ImageLoader.load(entry.getValue()));
         }
-        return textureMap;
     }
 
 }
