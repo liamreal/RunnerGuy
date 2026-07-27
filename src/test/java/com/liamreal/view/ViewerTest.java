3e8a8fb752d08885c9fc887fd92dc041b9873f6e
liamreal <liam.kerrin@ucdconnect.ie>
2026-07-27 21:13:36 +0200

    added Viewer tests, need to at one point look through tests and make sure im mocking static objects like config, because realised other tests may be being affected

---
+++ src/test/java/com/liamreal/view/ViewerTest.java
@@ -0,0 +1,80 @@
+package com.liamreal.view;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import com.liamreal.user.Config;
+import com.liamreal.game.Model;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+
+public class ViewerTest {
+    private MockedStatic<Config> mockedConfig;
+    private Config mockConfig;
+
+    @BeforeEach
+    public void setup() {
+        mockConfig = mock(Config.class);
+
+        mockedConfig = mockStatic(Config.class);
+
+        mockedConfig.when(Config::getInstance)
+                    .thenReturn(mockConfig);
+
+        when(mockConfig.getResolutionX())
+                .thenReturn(1280);
+
+        when(mockConfig.getResolutionY())
+                .thenReturn(720);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        mockedConfig.close();
+    }
+
+    @Test
+    public void testPreferredSize() {
+        Model world = new Model();
+
+        Viewer viewer = new Viewer(world);
+
+        Dimension size = viewer.getPreferredSize();
+
+        assertEquals(1280, size.width);
+        assertEquals(720, size.height);
+    }
+
+    @Test
+    public void testUpdateViewRepaints() {
+        Model world = new Model();
+
+        Viewer viewer = spy(new Viewer(world));
+
+        viewer.updateView();
+
+        verify(viewer).repaint();
+    }
+
+    @Test
+    public void testPaintComponentWithNoEntities() {
+        Model world = new Model();
+
+        Viewer viewer = new Viewer(world);
+
+        Graphics graphics = mock(Graphics.class);
+
+        assertDoesNotThrow(() -> {
+            viewer.paintComponent(graphics);
+        });
+    }
+}
