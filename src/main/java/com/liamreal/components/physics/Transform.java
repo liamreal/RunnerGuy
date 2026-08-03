f5d853d18c3834f7c91d30bba40b55a8630dbf20
liamreal <liam.kerrin@ucdconnect.ie>
2026-08-03 17:57:24 +0200

    removed duplication in Transform compareTo method which was already in Vector2f

--- src/main/java/com/liamreal/components/physics/Transform.java
+++ src/main/java/com/liamreal/components/physics/Transform.java
@@ -14,16 +14,9 @@ public class Transform implements Component, Comparable<Transform> {
         this.position = position;
     }
 
-    // comparsion methods
-    public int compareTo(Transform other) {
-        int i = this.compareX(other);
-		if (i != 0) return i;
-
-		i = this.compareY(other);
-		return i;
-    }
-    public int compareX(Transform other) { return this.getPosition().compareX(other.getPosition()); }
-    // return based on Y-position, if lower (higher on screen), should be first
+    // comparison delegating to Vector2f both coordinates comparison
+    public int compareTo(Transform other) { return this.getPosition().compareTo(other.getPosition()); }
+    // return based on Y-position, if lower (higher on screen), should be first, X not required as that is not relevant here
     public int compareY(Transform other) { return this.getPosition().compareY(other.getPosition()); }
 
     // add displacement vector (i.e. speed and direction calculated from velocity) to position
